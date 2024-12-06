7abb8ab4e9b7b7b83386a7512ddd54106608f362
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-12-06 21:21:17 +0200

    Update admin_cont.js

--- controllers/admin_cont.js
+++ controllers/admin_cont.js
@@ -25,16 +25,16 @@ const Register = async (req, res) => {
     const user = req.body;
     const dublicatedEmail = await Admin.findOne({ email: user.email });
     if (dublicatedEmail) {
-      return res.status(400).send("Email already exist!!");
+      return res.status(400).json("Email already exist!!");
     }
     const newUser = new Admin({...user,photo:link});
 
     await newUser.save();
 
   
-    res.status(200).send("Register is success !!");
+    res.status(200).json("Register is success !!");
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json(error.message);
   }
 };
 
@@ -44,11 +44,11 @@ const Login = async (req, res) => {
     const { email, password } = req.body;
     const user = await Admin.findOne({ email: email });
     if (!user) {
-      return res.status(404).send("EMAIL OR PASSWORD NOT CORRECT ");
+      return res.status(404).json("EMAIL OR PASSWORD NOT CORRECT ");
     }
     const isPassword = await bcryptjs.compare(password, user.password);
     if (!isPassword) {
-      return res.status(404).send("EMAIL OR PASSWORD NOT CORRECT ");
+      return res.status(404).json("EMAIL OR PASSWORD NOT CORRECT ");
     }
 
     const SECRETKEY = process.env.SECRETKEY;
@@ -63,9 +63,9 @@ const Login = async (req, res) => {
   
     res
       .status(200)
-      .send({ access_token: `Bearer ${token}`, success: "Login is success!" });
+      .json({ access_token: `Bearer ${token}`, success: "Login is success!" });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json(error.message);
   }
 };
 
@@ -73,11 +73,11 @@ const get_employee_not_verified = async(req,res) => {
   try{
     const users = await Employee.find({ verified: false });
     if (!users) {
-      return res.status(404).send("No employees found!");
+      return res.status(404).json("No employees found!");
     }
-    res.status(200).send(users);
+    res.status(200).json(users);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 }
 const verifyEmployeeEmail = async (req, res) => {
@@ -85,12 +85,12 @@ const verifyEmployeeEmail = async (req, res) => {
     const employee_id = req.params.employee_id;
     const user = await Employee.findByIdAndUpdate(employee_id,{ verified: true ,new:true});
     if (!user) {
-      return res.status(404).send("User not found!");
+      return res.status(404).json("User not found!");
     }
    await user.save();
-    res.status(200).send("Email is verified!");
+    res.status(200).json("Email is verified!");
   } catch (e) {
-    res.status(500).send(e.message);  
+    res.status(500).json(e.message);  
   }
 };
 
@@ -99,36 +99,36 @@ const remove_verifyEmployeeEmail = async (req, res) => {
     const employee_id = req.params.employee_id;
     const user = await Employee.findByIdAndUpdate(employee_id,{ verified: false ,new:true});
     if (!user) {
-      return res.status(404).send("User not found!");
+      return res.status(404).json("User not found!");
     }
    await user.save();
-    res.status(200).send("Email is Not_verified!");
+    res.status(200).json("Email is Not_verified!");
   } catch (e) {
-    res.status(500).send(e.message);  
+    res.status(500).json(e.message);  
   }
 };
 const getEmployee = async (req, res) => {
   try {
     const employee_id  = req.params.employee_id;
     if (!mongoose.Types.ObjectId.isValid(employee_id)) {
-      return res.status(400).send(" ID is not correct!");
+      return res.status(400).json(" ID is not correct!");
     }
     const user = await Employee.findById(employee_id);
     if (!user) {
-      return res.status(404).send(" please SignUp ");
+      return res.status(404).json(" please SignUp ");
     }
-    res.status(200).send(user);
+    res.status(200).json(user);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
 const getAllEmployee = async (req, res) => {
   try {
     const allData = await Employee.find();
-    res.status(200).send(allData);
+    res.status(200).json(allData);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
@@ -136,11 +136,11 @@ const editAdminData = async (req, res) => {
   try {
     const id = req.user._id
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).send("ID is not correct!!");
+      return res.status(404).json("ID is not correct!!");
     }
     const data=await Admin.findById(id)
     if (!data) {
-      return res.status(404).send("User not found!");
+      return res.status(404).json("User not found!");
     }
     const file = req.files?.find(f => f.fieldname === 'file');
          let link ;
@@ -151,7 +151,7 @@ const editAdminData = async (req, res) => {
              await Admin.findByIdAndUpdate(id,{...req.body,photo:link, new: true })
             await data.save();
 
-            return res.status(200).send("Data updated successfully!");
+            return res.status(200).json("Data updated successfully!");
 
         }
 
@@ -159,9 +159,9 @@ const editAdminData = async (req, res) => {
 
         await Admin.findByIdAndUpdate(id,{...req.body,new: true })
         await data.save();;
-    res.status(200).send("Data updated successfully!");
+    res.status(200).json("Data updated successfully!");
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
@@ -170,14 +170,14 @@ const get_all_done_tasks = async (req, res) => {
     const user_data = await Admin.findById(req.user._id);
       
     if (!user_data.isAdmin ) {
-          return res.status(400).send('You are not a Admin!');
+          return res.status(400).json('You are not a Admin!');
       }
 
    const all_tasks = await DoneTask.find()
    
-    res.status(200).send(all_tasks);
+    res.status(200).json(all_tasks);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
@@ -187,14 +187,14 @@ const get_det_done_task = async (req, res) => {
     const user_data = await Admin.findById(req.user._id);
       
     if (!user_data.isAdmin ) {
-          return res.status(400).send('You are not a Admin!');
+          return res.status(400).json('You are not a Admin!');
       }
 
    const done_task = await DoneTask.findById(task_id)
    
-    res.status(200).send(done_task);
+    res.status(200).json(done_task);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
@@ -228,10 +228,10 @@ const get_tasks_nearly_not_done = async (req, res) => {
     });
 
     
-    res.status(200).send(tasks);
+    res.status(200).json(tasks);
   } catch (e) {
     
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
@@ -242,25 +242,25 @@ const add_access_manager=async (req, res) => {
     const user_data = await Admin.findById(req.user._id);
       
     if (!user_data.isAdmin ) {
-          return res.status(400).send('You are not a Admin!');
+          return res.status(400).json('You are not a Admin!');
       }
 
       const employee_id = req.params.employee_id
       const employee_data = await Employee.findById(employee_id);
       
       if (!employee_data) {
-        return res.status(404).send('Employee not found!');
+        return res.status(404).json('Employee not found!');
       }
      
       await Employee.findByIdAndUpdate(employee_id,{isManager: true,new:true})
 
       await employee_data.save()
 
-       res.status(200).send(`${employee_data.name} is now a manager !!`);
+       res.status(200).json(`${employee_data.name} is now a manager !!`);
 
 
     }catch (e) {
-    res.status(500).send(e.message);}
+    res.status(500).json(e.message);}
 }
 
 const remove_access_manager=async (req, res) => {
@@ -269,25 +269,25 @@ const remove_access_manager=async (req, res) => {
     const user_data = await Admin.findById(req.user._id);
       
     if (!user_data.isAdmin ) {
-          return res.status(400).send('You are not a Admin!');
+          return res.status(400).json('You are not a Admin!');
       }
 
       const employee_id = req.params.employee_id
       const employee_data = await Employee.findById(employee_id);
       
       if (!employee_data) {
-        return res.status(404).send('Employee not found!');
+        return res.status(404).json('Employee not found!');
       }
      
       await Employee.findByIdAndUpdate(employee_id,{isManager: false,new:true})
 
       await employee_data.save()
 
-       res.status(200).send(`${employee_data.name} is now a not_manager !!`);
+       res.status(200).json(`${employee_data.name} is now a not_manager !!`);
 
 
     }catch (e) {
-    res.status(500).send(e.message);}
+    res.status(500).json(e.message);}
 }
 
 const deleteEmployee = async (req, res) => {
@@ -297,7 +297,7 @@ const deleteEmployee = async (req, res) => {
    
     const employee = await Employee.findById(employeeId);
     if (!employee) {
-      return res.status(404).send('Employee not found!');
+      return res.status(404).json('Employee not found!');
     }
 
    
@@ -315,7 +315,7 @@ const deleteEmployee = async (req, res) => {
    
     await Chat.deleteMany({ 
       $or: [
-        { sender: employeeId }, 
+        { jsoner: employeeId }, 
         { receiver: employeeId }
       ]
     });
@@ -323,9 +323,9 @@ const deleteEmployee = async (req, res) => {
 
     await Employee.findByIdAndDelete(employeeId);
 
-    res.status(200).send('Employee deleted successfully.');
+    res.status(200).json('Employee deleted successfully.');
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json(error.message);
   }
 };
 
@@ -336,15 +336,15 @@ const deleteDoneTask = async (req, res) => {
     
     const doneTask = await DoneTask.findById(doneTaskId);
     if (!doneTask) {
-      return res.status(404).send('DoneTask not found!');
+      return res.status(404).json('DoneTask not found!');
     }
 
     
     await DoneTask.findByIdAndDelete(doneTaskId);
 
-    res.status(200).send('DoneTask deleted successfully.');
+    res.status(200).json('DoneTask deleted successfully.');
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json(error.message);
   }
 };
 
@@ -354,25 +354,25 @@ const add_block=async (req, res) => {
     const user_data = await Admin.findById(req.user._id);
       
     if (!user_data.isAdmin ) {
-          return res.status(400).send('You are not a Admin!');
+          return res.status(400).json('You are not a Admin!');
       }
 
       const employee_id = req.params.employee_id
       const employee_data = await Employee.findById(employee_id);
       
       if (!employee_data) {
-        return res.status(404).send('Employee not found!');
+        return res.status(404).json('Employee not found!');
       }
      
       await Employee.findByIdAndUpdate(employee_id,{isBlock: true,new:true})
 
       await employee_data.save()
 
-       res.status(200).send(`${employee_data.name} is now BLOCKED !!`);
+       res.status(200).json(`${employee_data.name} is now BLOCKED !!`);
 
 
     }catch (e) {
-    res.status(500).send(e.message);}
+    res.status(500).json(e.message);}
 }
 
 const remove_block=async (req, res) => {
@@ -381,31 +381,31 @@ const remove_block=async (req, res) => {
     const user_data = await Admin.findById(req.user._id);
       
     if (!user_data.isAdmin ) {
-          return res.status(400).send('You are not a Admin!');
+          return res.status(400).json('You are not a Admin!');
       }
 
       const employee_id = req.params.employee_id
       const employee_data = await Employee.findById(employee_id);
       
       if (!employee_data) {
-        return res.status(404).send('Employee not found!');
+        return res.status(404).json('Employee not found!');
       }
      
       await Employee.findByIdAndUpdate(employee_id,{isBlock: false,new:true})
 
       await employee_data.save()
 
-       res.status(200).send(`${employee_data.name} is now a NOT_BLOCKED !!`);
+       res.status(200).json(`${employee_data.name} is now a NOT_BLOCKED !!`);
 
 
     }catch (e) {
-    res.status(500).send(e.message);}
+    res.status(500).json(e.message);}
 }
 const delete_all_taskings= async(req,res)=>{
   try{
  await Task.deleteMany()
- res.status(200).send('All Tasking deleted successfully!')
-  }catch(e){res.status(500).send(e.message)}
+ res.status(200).json('All Tasking deleted successfully!')
+  }catch(e){res.status(500).json(e.message)}
 }
 
 const get_employees_ref_section = async (req, res) => {
@@ -413,9 +413,9 @@ const get_employees_ref_section = async (req, res) => {
     const section_name = req.body.section_name;
     const sectionToLowerCase = section_name.toLowerCase();
     const employees = await Employee.find({ section: sectionToLowerCase });
-    res.status(200).send(employees);
+    res.status(200).json(employees);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
@@ -425,12 +425,12 @@ const edit_in_employee_data = async(req, res)=> {
     const user_data = await Employee.findByIdAndUpdate(employee_id ,{...req.body, new: true });
 
     if (!user_data) {
-      return res.status(404).send('Employee not found!');
+      return res.status(404).json('Employee not found!');
     }
     await user_data.save();
-    res.status(200).send(user_data);
+    res.status(200).json(user_data);
   } catch (e) {
-    res.status(500).send(e.message);
+    res.status(500).json(e.message);
   }
 };
 
