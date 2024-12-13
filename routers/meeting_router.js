e0db8d29951c727ef7f8c7f6526f406d9c7a5d13
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-12-14 00:29:01 +0200

    Update meeting_router.js

--- routers/meeting_router.js
+++ routers/meeting_router.js
@@ -11,6 +11,7 @@ const{
     delete_meeting,
     update_meeting,
     delete_all_meeting,
+    get_metting_in_section,
     //employee
     get_employee_meetings,
     get_employee_det_meeting,}=require("../controllers/meeting_cont")
@@ -19,6 +20,7 @@ router.post("/admin/create_meeting",adminAuth,admin_add_metting)
 router.post("/create_meeting",auth,manager_add_metting)
 router.get("/get_all_meetings",get_all_meetings)
 router.get("/get_det_meeting/:meeting_id",get_det_meeting)
+router.get("/get_meeting_in_section",get_metting_in_section)
 router.delete("/delete_meeting/:meeting_id",delete_meeting)
 router.patch("/edit_meeting/:meeting_id",update_meeting)
 router.delete("/delete_all_meetings",delete_all_meeting)
