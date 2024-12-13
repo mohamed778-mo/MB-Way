c4706a1870b4849ac8971d8f39d9d7d1e562da8a
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-12-14 00:55:33 +0200

    Update dashboard_router.js

--- routers/website/dashboard_router.js
+++ routers/website/dashboard_router.js
@@ -13,17 +13,17 @@ const{
     delete_all_services,
 }=require("../../controllers/website/dashboard(website)_control")
 const Istorage=require("../../middleware/multer")
-const {adminAuth}=require("../../middleware/auth")
+const {auth}=require("../../middleware/auth")
 
 router.post('/register',dash_register)
 router.post('/login',dash_admin_Login)
-router.put('/add_section',adminAuth,Istorage.any(),add_section)
-router.put('/edit_section/section_id',adminAuth,Istorage.any(),edit_section)
-router.post('/get_all_sections',adminAuth,get_all_sections)
-router.get('/get_section/:section_id',adminAuth,get_section)
-router.get('/get_service/:section_id/:service_id',adminAuth,get_service)
-router.get('/delete_service/:section_id/:service_id',adminAuth,delete_service)
-router.get('/delete_all_services',adminAuth,delete_all_services)
+router.put('/add_section',auth,Istorage.any(),add_section)
+router.put('/edit_section/section_id',auth,Istorage.any(),edit_section)
+router.post('/get_all_sections',auth,get_all_sections)
+router.get('/get_section/:section_id',auth,get_section)
+router.get('/get_service/:section_id/:service_id',auth,get_service)
+router.get('/delete_service/:section_id/:service_id',auth,delete_service)
+router.get('/delete_all_services',auth,delete_all_services)
 
 module.exports = router
 
