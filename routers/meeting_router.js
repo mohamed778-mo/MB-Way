cdc8d913fe543fab248357e528983b0802b34027
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-12-14 00:54:25 +0200

    Update meeting_router.js

--- routers/meeting_router.js
+++ routers/meeting_router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const {auth,adminAuth}=require("../middleware/auth")
+const {auth}=require("../middleware/auth")
 
 const{
     admin_add_metting,
@@ -16,7 +16,7 @@ const{
     get_employee_meetings,
     get_employee_det_meeting,}=require("../controllers/meeting_cont")
 
-router.post("/admin/create_meeting",adminAuth,admin_add_metting)
+router.post("/admin/create_meeting",auth,admin_add_metting)
 router.post("/create_meeting",auth,manager_add_metting)
 router.get("/get_all_meetings",get_all_meetings)
 router.get("/get_det_meeting/:meeting_id",get_det_meeting)
