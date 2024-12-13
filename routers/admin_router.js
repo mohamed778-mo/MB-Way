6de7a4abd42c2ba91347b38b60fd0ace746fa058
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-12-14 00:29:40 +0200

    Update admin_router.js

--- routers/admin_router.js
+++ routers/admin_router.js
@@ -1,79 +1,66 @@
-const express = require("express")
-const router = express.Router()
-const Istorage =require("../middleware/multer")
+const express = require("express");
+const router = express.Router();
+const Istorage = require("../middleware/multer");
+const {  auth } = require("../middleware/auth"); 
 
-const {adminAuth}=require("../middleware/auth")
-
-const{
-    Register, 
-    getEmployee,
-    getAllEmployee,
-    editAdminData,
-    get_tasks_nearly_not_done,
-    get_all_done_tasks,
-    get_det_done_task,
-    add_access_manager,
-    remove_access_manager,
-    deleteEmployee,
-    deleteDoneTask,
-    add_block,
-    remove_block,
-    delete_all_taskings,
-    get_employee_not_verified,
-    verifyEmployeeEmail,
-   remove_verifyEmployeeEmail,
-   get_employees_ref_section,
-   edit_in_employee_data,
-     delete_nearly_Task,
+const {
+  Register,
+  add_task,
+  delete_Task,
+  get_all_notdone_tasks,
+  not_done_tasks_in_section,
+  done_tasks_in_section,
+  getEmployee,
+  getAllEmployee,
+  editAdminData,
+  get_tasks_nearly_not_done,
+  get_all_done_tasks,
+  get_det_done_task,
+  deleteEmployee,
+  deleteDoneTask,
+  get_employee_not_verified,
+  verifyEmployeeEmail,
+  remove_verifyEmployeeEmail,
+  get_employees_ref_section,
+  edit_employee_data,
   get_det_notdone_task,
-  update_task
+  update_task,
+  delete_all_done_taskings,
+  delete_all_not_done_taskings   
+} = require("../controllers/admin_cont");
 
-}=require("../controllers/admin_cont")
+router.post('/register', Istorage.any(), Register); //admin
 
-router.post('/register',Istorage.any(),Register)
+router.post('/add_task', auth, Istorage.any(), add_task); //admin manager
+router.patch('/edit_not_done_task/:task_id', auth, update_task); //admin manager
+router.put('/not_done_tasks_in_section', auth, not_done_tasks_in_section); //admin manager
+router.put('/done_tasks_in_section', auth, done_tasks_in_section); //admin manager
+router.get('/get_det_done_task/:task_id', auth, get_det_done_task); //admin manager
+router.get('/get_det_not_done_task/:task_id', auth, get_det_notdone_task); //admin manager
+router.delete('/delete_done_task/:task_id', auth, deleteDoneTask);//admin manager
+router.delete('/delete_not_done_task/:task_id',auth, delete_Task);//admin manager
 
-router.get('/get_employee_not_verified',get_employee_not_verified)
-
-router.patch('/verify_email/:employee_id',verifyEmployeeEmail)
-
-router.patch('/remove_verify_email/:employee_id',remove_verifyEmployeeEmail)
-
-router.get('/get_employee/:employee_id',adminAuth,getEmployee)
-
-router.get('/get_all_employee',adminAuth,getAllEmployee)
-
-router.put('/edit_admin_data',adminAuth,Istorage.any(),editAdminData)
-
-router.get('/get_tasks_nearly_not_done',adminAuth,get_tasks_nearly_not_done)
-
-router.get('/get_all_done_tasks',adminAuth,get_all_done_tasks)
-
-router.get('/get_det_done_task/:task_id',adminAuth,get_det_done_task)
-
-router.patch('/add_access_manager/:employee_id',adminAuth,add_access_manager)
-
-router.patch('/remove_access_manager/:employee_id',adminAuth,remove_access_manager)
-
-router.delete('/delete_employee/:employee_id',adminAuth,deleteEmployee)
-
-router.delete('/delete_done_task/:task_id',adminAuth,deleteDoneTask)
-
-router.patch('/add_block/:employee_id',adminAuth,add_block)
-
-router.patch('/remove_block/:employee_id',adminAuth,remove_block)
-
-router.delete('/delete_all_taskings',adminAuth,delete_all_taskings)
-
-router.put('/get_employees_ref_section',get_employees_ref_section)
-
-router.patch('/edit_in_employee_data/:employee_id',edit_in_employee_data)
-
-router.get('/get_det_not_done_task/:task_id',get_det_notdone_task)
-
-router.patch('/edit_not_done_task/:task_id',update_task)
-
-router.delete('/delete_not_done_task/:task_id',delete_nearly_Task)
+router.get('/get_all_notdone_tasks', auth, get_all_notdone_tasks); //admin 
+router.get('/get_tasks_nearly_not_done', auth, get_tasks_nearly_not_done); //admin 
+router.get('/get_all_done_tasks', auth, get_all_done_tasks); //admin 
+router.delete('/delete_all_done_taskings', auth, delete_all_done_taskings); //admin
+router.delete('/delete_all_not_done_taskings', auth, delete_all_not_done_taskings); //admin
 
 
 
-module.exports = router
+
+router.patch('/verify_email/:employee_id', auth,verifyEmployeeEmail); //admin 
+router.patch('/remove_verify_email/:employee_id',auth, remove_verifyEmployeeEmail);//admin 
+router.get('/get_employees_not_verified',auth, get_employee_not_verified);//admin 
+router.get('/get_all_employee', auth, getAllEmployee);//admin 
+router.put('/edit_admin_data', auth, Istorage.any(), editAdminData); //admin personal data
+
+
+router.get('/get_employee/:employee_id', auth, getEmployee);//admin manager
+router.put('/edit_employee_data/:employee_id', auth, edit_employee_data);//admin manager
+router.delete('/delete_employee/:employee_id', auth, deleteEmployee); //admin manager
+router.put('/get_employees_ref_section',auth, get_employees_ref_section); //admin manager
+
+
+
+module.exports = router;
