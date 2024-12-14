3fdf0176b87c3a956e0aa79175679c9f3f50a4c5
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2024-12-14 23:13:01 +0200

    Update form_router.js

--- routers/form_router.js
+++ routers/form_router.js
@@ -4,28 +4,24 @@ const router = express.Router()
 const Istorage=require("../middleware/multer")
 
 const{
-    add_form,
-    get_forms,
-    get_form_by_id,
-    update_form,
-    delete_form,
-    delete_all_forms,
-    get_my_forms_by_employee_id
+    add_medical_form,
+    get_forms_available,
+    get_medical_forms,
+    get_form_medical_by_id,
+    update_medical_form,
+    delete_medical_form,
+    delete_medical_all_forms,
+    get_my_forms_medical_by_employee_id
 }=require("../controllers/form_cont")
 
-router.post("/add_form",Istorage.any(),add_form)
-
-router.get("/get_forms",get_forms)
-
-router.get("/get_form/:form_id",get_form_by_id)
-
-router.put("/update_form/:form_id",update_form)
-
-router.delete("/delete_form/:form_id",delete_form)
-
-router.delete("/delete_all_forms",delete_all_forms)
-
-router.get("/get_my_forms/:employee_id",get_my_forms_by_employee_id)
+router.post("/add_form",Istorage.any(),add_medical_form)
+router.get("/get_forms_available",get_forms_available)
+router.get("/get_forms",get_medical_forms)
+router.get("/get_form/:form_id",get_form_medical_by_id)
+router.put("/update_form/:form_id",update_medical_form)
+router.delete("/delete_form/:form_id",delete_medical_form)
+router.delete("/delete_all_forms",delete_medical_all_forms)
+router.get("/get_my_forms/:employee_id",get_my_forms_medical_by_employee_id)
 
 
 module.exports = router
