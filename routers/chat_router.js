785b098b0086d4ee3aef3880b0a9a427dc4c338f
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-01-24 21:38:35 +0200

    Update chat_router.js

--- routers/chat_router.js
+++ routers/chat_router.js
@@ -4,12 +4,12 @@ const router = express.Router()
 const{ addMessage ,getMessages,deleteChat,markMessagesAsRead}=require("../controllers/chat")
 const Istorage=require("../middleware/multer")
 
-router.post('/add_message/:senderId/:receiverId',Istorage.any(),addMessage)
+router.post("/add_message/:senderId/:receiverId",Istorage.any(),addMessage)
  
-router.get('/get_messages/:userIdReceiver/:userIdSender',getMessages)
+router.get("/get_messages/:userIdReceiver/:userIdSender",getMessages)
 
-router.delete('/delete_chat/:senderId/:receiverId',deleteChat)
+router.delete("/delete_chat/:senderId/:receiverId", deleteChat)
 
-router.patch('/mark_messages_as_read/:userIdReceiver/:userIdSender',markMessagesAsRead)
+router.patch("/mark_messages_as_read/:userIdReceiver/:userIdSender", markMessagesAsRead)
 
 module.exports = router
