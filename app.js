121d836f35cdb987cf357f1ac58912a2b1b3bb54
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-01-24 21:24:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
-const cluster = require('cluster'); 
-const os = require('os'); 
+const cluster = require('cluster');
+const os = require('os');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const connection = require("./config/config");
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const path = require("path");
-const http = require('http'); 
-const { Server } = require('socket.io'); 
-
+const connection = require('./config/config');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const numCPUs = os.cpus().length;
-
 
 if (cluster.isMaster) {
     console.log(`Master process is running with PID: ${process.pid}`);
 
-  
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork(); 
-    }
 
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
     cluster.on('exit', (worker, code, signal) => {
         console.log(`Worker ${worker.process.pid} died. Spawning a new one...`);
-        cluster.fork(); 
+        cluster.fork();
     });
 } else {
-   
     const app = express();
 
    
     app.use(helmet());
 
   
+    app.use(cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    }));
+
+
+    app.use(bodyParser.json({ limit: '1gb', extended: true }));
+    app.use(bodyParser.urlencoded({ limit: '1gb', extended: true }));
+
+  
+    app.use(cookieParser());
+
+ 
+    connection();
+
+
     const server = http.createServer(app);
     const io = new Server(server, {
         cors: {
@@ -44,22 +57,7 @@ if (cluster.isMaster) {
         },
     });
 
-   
-    app.use(express.json({ limit: '1gb' }));
 
-    
-    app.use(cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
-        allowedHeaders: ['Content-Type', 'Authorization'], 
-    }));
-
-   
-    app.use(cookieParser());
-    app.use(bodyParser.json({ limit: "1gb", extended: true }));
-    app.use(bodyParser.urlencoded({ limit: "1gb", extended: true }));
-
-  
     const adminRouter = require('./routers/admin_router');
     const employeeRouter = require('./routers/empolyee_router');
     const chatRouter = require('./routers/chat_router');
@@ -68,10 +66,8 @@ if (cluster.isMaster) {
     const FRpasswordRouter = require('./routers/forgretpassword_router');
     const loginRouter = require('./routers/login_router');
     const engRouter = require('./routers/Eng_router');
-
     const dashboardRouter = require('./routers/website/dashboard_router');
 
-   
     app.use('/app/choice', loginRouter);
     app.use('/app/chat', chatRouter);
     app.use('/app/admin', adminRouter);
@@ -80,22 +76,15 @@ if (cluster.isMaster) {
     app.use('/app/form', formRouter);
     app.use('/app/user', FRpasswordRouter);
     app.use('/app/eng', engRouter);
-
-
-
     app.use('/app/dashboard', dashboardRouter);
 
-    
     app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-    
-    connection();
-
-  
+   
     io.on('connection', (socket) => {
         console.log('A user connected:', socket.id);
 
-        
+      
         socket.on('joinRoom', (chatId) => {
             socket.join(chatId);
         });
@@ -105,19 +94,17 @@ if (cluster.isMaster) {
             io.to(chatId).emit('receiveMessage', { sender, content, timestamp: new Date() });
         });
 
-        
+    
         socket.on('disconnect', () => {
             console.log('A user disconnected:', socket.id);
         });
     });
 
-  
+
     const port = 3000;
     server.listen(port, () => {
         console.log(`Worker server is running on port ${port} with PID: ${process.pid}`);
     });
+
+    module.exports = app;
 }
-
-
-module.exports = app;
-
