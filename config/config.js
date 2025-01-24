9dacc6cbf4834c7d7711c49df7fa0676d54d12a6
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-01-24 21:14:18 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,11 @@ const mongoose =require("mongoose")
 require("dotenv").config()
 const URL =process.env.MONGODB_URL
 const connection = ()=>{
-    mongoose.connect(URL)
+   mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
     .then(()=>{console.log('done connection !!')})
     .catch((error)=>{console.log(error.message)})
 }
