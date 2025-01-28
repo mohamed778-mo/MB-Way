181f1068be6a8c029bac016d407c8ebe3a1c7022
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-01-28 22:33:22 +0200

    Update form_buy_equi_model.js

--- models/eng/form_buy_equi_model.js
+++ models/eng/form_buy_equi_model.js
@@ -13,10 +13,53 @@ let BuyequipmentSchema = new mongoose.Schema({
         required:true
     },
     equipment:[{
-        equipment_id: {
+    equipment_id: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Equipment',
         },
+    equipment_name: {
+        type: String,
+        required: true,
+    },
+    equipment_type: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    model: {
+        type: String,
+    },
+    serial_number: {
+        type: String,
+    },
+    brand: {
+        type: String,
+    },
+    start: {
+        type: Date,
+    },
+    end: {
+        type: Date,
+    },
+    price: {
+        type: String,
+    },
+    quantity: {
+        type: Number,
+    },
+    warranty_duration: {
+        type: String,
+    },
+    condition_equipment: {
+        type: String,
+    },
+    which_is_covered_by_the_warranty:{
+        type: String,
+    },
+    the_party_responsible_for_the_guarantee:{
+        type: String,
+    }
     }
 ],
 
