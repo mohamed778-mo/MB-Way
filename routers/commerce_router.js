67736bd03fb9966831ee9930d0fc9ba574a477b2
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-01-29 23:56:18 +0200

    Add files via upload

---
+++ routers/commerce_router.js
@@ -0,0 +1,63 @@
+const express=require('express');
+const router = express.Router();
+
+const {auth}=require('../middleware/auth')
+const Istorage=require("../middleware/multer")
+
+const  {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct,
+
+    createSupplier,
+    getAllSuppliers,
+    getSupplierById,
+    updateSupplier,
+    deleteSupplier,
+
+    createRequest,
+    getAllRequests,
+    getRequestById,
+    updateRequest,
+    deleteRequest,
+
+} = require("../controllers/form_commerce_cont");
+
+router.post('/create_product',auth,Istorage.any(),createProduct);
+
+router.get('/get_all_products',auth,getAllProducts);
+
+router.get('/get_product/:id',auth,getProductById);
+
+router.put('/update_product/:id',auth,Istorage.any(),updateProduct);
+
+router.delete('/delete_product/:id',auth,deleteProduct);
+
+///////////////////////////////////////////////////////////////////
+
+router.post('/create_supplier',auth,Istorage.any(),createSupplier);
+
+router.get('/get_all_suppliers',auth,getAllSuppliers);
+
+router.get('/get_supplier/:id',auth,getSupplierById);
+
+router.put('/update_supplier/:id',auth,Istorage.any(),updateSupplier);
+
+router.delete('/delete_supplier/:id',auth,deleteSupplier);
+
+//////////////////////////////////////////////////////////////////////
+
+router.post('/create_request',auth,createRequest);
+
+router.get('/get_all_requests',auth,getAllRequests);
+
+router.get('/get_request/:id',auth,getRequestById);
+
+router.put('/update_supplier/:id',auth,updateSupplier);
+
+
+
+
+module.exports = router
