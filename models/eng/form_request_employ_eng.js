02eb64693cd5337fe20518edd0fe12d8d3fd4d33
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-01-29 05:22:22 +0200

    Update form_request_employ_eng.js

--- models/eng/form_request_employ_eng.js
+++ models/eng/form_request_employ_eng.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const validator=require('validator')
 let EmployEngSchema = new mongoose.Schema({
     company_name: {
         type: String,
@@ -13,6 +13,12 @@ let EmployEngSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        trim:true,
+        validate(valu){
+            if(!validator.isEmail(valu)){
+                throw new Error("Invalid email")
+            }
+        }
 
     },
     phone: {
