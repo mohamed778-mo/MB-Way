b3485e40edf90f97867a65b428509030963110a4
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-07 21:21:07 +0200

    Update login_router.js

--- routers/login_router.js
+++ routers/login_router.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
-const{ Login }=require("../controllers/login_api")
+const{ Login , getProfile }=require("../controllers/login_api")
+
+const {auth}=require("../middleware/auth.js")
 
 router.post('/login',Login)
+
+router.get('/get_profile',auth,getProfile)
+
 
 module.exports = router
