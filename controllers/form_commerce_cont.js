91f13919a27bd0b0d0541ac42dd94454c622dba8
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-07 13:42:56 +0200

    Update form_commerce_cont.js

--- controllers/form_commerce_cont.js
+++ controllers/form_commerce_cont.js
@@ -54,8 +54,8 @@ const getAllProducts = async (req, res) => {
         if (!user || !user.isManager) {
             return res.status(403).json("You are not accessing this page!");
         }
-        const choice = req.body.choice
-        const products = await Product.find({choice:choice});
+       
+        const products = await Product.find();
         res.status(200).json(products);
     } catch (error) {
         res.status(500).json(error.message);
