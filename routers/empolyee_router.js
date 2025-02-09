0c2ac68e77d80ec4d015c100de983305525c08ec
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-09 19:49:58 +0200

    Update empolyee_router.js

--- routers/empolyee_router.js
+++ routers/empolyee_router.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const{
     Register,
-    editEmployeeData,
     get_employee_tasks,
     get_employee_det_task,
     attach_employee_task,
@@ -12,7 +11,6 @@ const Istorage=require("../middleware/multer")
 const {auth}=require("../middleware/auth")
 
 router.post('/register',Istorage.any(),Register)
-router.put('/edit_employee_data',auth,Istorage.any(),editEmployeeData)
 router.get('/get_employee_tasks',auth,get_employee_tasks)
 router.get('/get_employee_det_task/:task_id',auth,get_employee_det_task)
 router.post('/attach_employee_task/:task_id',auth,attach_employee_task)
