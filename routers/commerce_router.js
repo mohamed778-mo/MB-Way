a774b8ee7f59e08a48dc0f7fbc32ab15afdb676a
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-09 17:37:50 +0200

    Update commerce_router.js

--- routers/commerce_router.js
+++ routers/commerce_router.js
@@ -62,10 +62,10 @@ router.put('/update_request/:id',auth,updateRequest);
 
 router.delete('/delete_request/:id',auth,deleteRequest);
 
-router.put('/update/tracking/:requestId/:productId',auth, updateStepsForRequest);
+router.put('/update/tracking/:requestId',auth, updateStepsForRequest);
 
 
-router.delete('/delete/tracking/:requestId/:productId',auth, deleteStepsForRequest);
+router.delete('/delete/tracking/:requestId',auth, deleteStepsForRequest);
 
 
 module.exports = router
