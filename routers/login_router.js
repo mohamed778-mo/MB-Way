4340e4af2bd6c393296cb580e29e5e1aed61f69e
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-09 19:51:23 +0200

    Update login_router.js

--- routers/login_router.js
+++ routers/login_router.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-const{ Login , getProfile , remove_my_account}=require("../controllers/login_api")
-
+const{ Login , getProfile , remove_my_account , editUserData }=require("../controllers/login_api")
+const Istorage=require("../middleware/multer")
 const {auth}=require("../middleware/auth.js")
 
 router.post('/login',Login)
@@ -10,6 +10,9 @@ router.post('/login',Login)
 router.get('/get_profile',auth,getProfile)
 
 router.delete('/remove_my_account',auth,remove_my_account)
+
+router.put('/edit_user_data', auth, Istorage.any(), editUserData); 
+// admin and employee edit on personal data
 
 
 
