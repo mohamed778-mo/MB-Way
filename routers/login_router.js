f4a5c7edb29f18096f343258cb85eb1c112dd5b7
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-09 18:45:35 +0200

    Update login_router.js

--- routers/login_router.js
+++ routers/login_router.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
-const{ Login , getProfile }=require("../controllers/login_api")
+const{ Login , getProfile , remove_my_account}=require("../controllers/login_api")
 
 const {auth}=require("../middleware/auth.js")
 
 router.post('/login',Login)
 
 router.get('/get_profile',auth,getProfile)
+
+router.delete('/remove_my_account',auth,remove_my_account)
+
 
 
 module.exports = router
