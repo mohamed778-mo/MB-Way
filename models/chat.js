be7c808d24684951e37c39a5ba3a2a921b700185
m.elmala <137796091+mohamed778-mo@users.noreply.github.com>
2025-02-10 18:49:54 +0200

    Update chat.js

--- models/chat.js
+++ models/chat.js
@@ -28,7 +28,7 @@ var chatSchema = new mongoose.Schema({
     isRead: { type: Boolean, default: false },
     attachment: String,
     sender: mongoose.Schema.Types.ObjectId,
-    me: Boolean 
+    
   }],
     timestamp: {
         type: Date,
